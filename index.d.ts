72fc33d4ec8353d70052fca934828c4fa1bcc9f3
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-19 00:27:00 +0200

    Require Node.js 8, add more extensions, add TypeScript definition

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+/**
+List of text file extensions.
+
+@example
+```
+import textExtensions = require('text-extensions');
+
+console.log(textExtensions);
+//=> ['asp', 'aspx', ...]
+```
+*/
+declare const textExtensions: readonly string[];
+
+export = textExtensions;
